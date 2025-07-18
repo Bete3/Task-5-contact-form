66608aa69e682be94616cba68da172ccf54b5a0f
Betelhem Yehuala <167322606+Bete3@users.noreply.github.com>
2025-07-18 11:33:15 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactForm from './components/ContactForm';
+
+const App = () => {
+  return (
+    <div style={{ padding: '30px' }}>
+      <ContactForm />
+    </div>
+  );
+};
+
+export default App;
